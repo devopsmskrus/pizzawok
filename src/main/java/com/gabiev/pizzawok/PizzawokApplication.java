2b25eaceecb4d9c7e9add04e14657f373c15432d
Gabi <gabi_gabiev@mail.ru>
2023-08-22 00:50:17 +0300

    init project

---
+++ src/main/java/com/gabiev/pizzawok/PizzawokApplication.java
@@ -0,0 +1,13 @@
+package com.gabiev.pizzawok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzawokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzawokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabiev/pizzawok/PizzawokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabiev.pizzawok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzawokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
