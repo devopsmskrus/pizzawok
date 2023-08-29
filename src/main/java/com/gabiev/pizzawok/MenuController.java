0fb8d5d3f09f4a95efa5e4aaaa9b653a2a8b949b
Gabi <gabi_gabiev@mail.ru>
2023-08-30 00:44:14 +0300

    Нарисовал "красивый" шаблон

---
+++ src/main/java/com/gabiev/pizzawok/MenuController.java
@@ -0,0 +1,25 @@
+package com.gabiev.pizzawok;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/menu")
+public class MenuController {
+
+//    @GetMapping("/pizza")
+//    public String getPizzaForm() {
+//        return "pizza";
+//    }
+//
+//    @GetMapping("/wok")
+//    public String getWokForm() {
+//        return "wok";
+//    }
+//
+//    @GetMapping("/roll")
+//    public String getRollForm() {
+//        return "roll";
+//    }
+}
